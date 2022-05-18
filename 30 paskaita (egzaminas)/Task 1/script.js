5346e6883539f035eca5f81e46efdf55ef7b709b
Laurynas <simonaitis.laurynas@gmail.com>
2022-05-18 20:50:42 +0300

    second task completed

--- 30 paskaita (egzaminas)/Task 1/script.js
+++ 30 paskaita (egzaminas)/Task 1/script.js
@@ -8,3 +8,35 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+let input = document.getElementById('search')
+const form  = document.getElementsByTagName('form')
+const output = document.getElementById('output')
+
+form[0].addEventListener('submit', (event) => {
+    event.preventDefault();
+    let number = input.value
+    let convertToLb = number * 2.2046
+    let convertToGrams = number / 0.0010000
+    let convertToOz = number * 35.274
+
+    let displayLb = document.createElement('div')
+    displayLb.setAttribute('class', 'displayLb')
+    const removeDisplayLb = document.getElementsByClassName("displayLb");
+    while (removeDisplayLb.length > 0) removeDisplayLb[0].remove();
+    output.append(displayLb)
+    displayLb.innerHTML = convertToLb + ' ' + 'lb'
+
+    let displayGrams = document.createElement('div')
+    displayGrams.setAttribute('class', 'displayGrams')
+    const removeDisplayGrams = document.getElementsByClassName("displayGrams");
+    while (removeDisplayGrams.length > 0) removeDisplayGrams[0].remove();
+    output.append(displayGrams)
+    displayGrams.innerHTML = convertToGrams + ' ' + 'g'
+
+    let displayOz = document.createElement('div')
+    displayOz.setAttribute('class', 'displayOz')
+    const removeDisplayOz = document.getElementsByClassName("displayOz");
+    while (removeDisplayOz.length > 0) removeDisplayOz[0].remove();
+    output.append(displayOz)
+    displayOz.innerHTML = convertToOz + ' ' + 'oz'
+});
