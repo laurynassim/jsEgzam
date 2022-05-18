1c4ad0c45815c0738ec87a17779d74acddfe7737
Laurynas <simonaitis.laurynas@gmail.com>
2022-05-18 23:34:28 +0300

    third task complete

--- 30 paskaita (egzaminas)/Task 3/script.js
+++ 30 paskaita (egzaminas)/Task 3/script.js
@@ -11,3 +11,56 @@ Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų 
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+let showButton = document.getElementById('btn')
+let displayDiv = document.getElementById('output')
+
+
+showButton.addEventListener('click', getDataFromApi)
+
+async function getDataFromApi() {
+    
+    document.getElementById('message').innerText = ''
+    let response = await fetch (ENDPOINT)
+    let data = await response.json()
+    showDataInDom(data)
+}
+
+function showDataInDom(data){
+    console.log(data)
+    
+
+    let displayDivWrapper = document.createElement('div')
+    displayDivWrapper.setAttribute('class', 'displayDivWrapper')
+    const removeDivWrapper = document.getElementsByClassName("displayDivWrapper");
+    while (removeDivWrapper.length > 0) removeDivWrapper[0].remove();
+    displayDiv.append(displayDivWrapper)
+
+    for (let i = 0; i < data.length; i++){
+        createTabs(data, displayDivWrapper, i)
+    }
+    
+    
+}
+
+function createTabs(array, parentDiv, index){
+    
+    
+
+    let tabsContainer =  document.createElement('div')
+    tabsContainer.setAttribute('class', 'tabsContainer')
+    parentDiv.append(tabsContainer)
+
+    let displayLogin = document.createElement('div')
+    displayLogin.setAttribute('clas', 'displayLogin')
+    tabsContainer.append(displayLogin)
+
+    let displayAvatarImg = document.createElement('img')
+    displayAvatarImg.setAttribute('class', 'displayAvatarImg')
+    tabsContainer.append(displayAvatarImg)
+
+    displayLogin.innerText = array[index].login
+    displayAvatarImg.src = String(array[index].avatar_url)
+
+
+
+}
