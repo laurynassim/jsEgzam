57d7eb56c1b655c73fcf45764abaca71d80bf56c
Laurynas <simonaitis.laurynas@gmail.com>
2022-05-19 22:00:19 +0300

    seventh task completed

--- 30 paskaita (egzaminas)/Task 7/script.js
+++ 30 paskaita (egzaminas)/Task 7/script.js
@@ -10,3 +10,13 @@ const audi = {
   year: 2005,
   color: 'white',
 };
+
+showObjectKeys(audi)
+
+function showObjectKeys(object){
+  let audiArray = []
+  for (let key in object){
+    audiArray.push(object[key])
+  }
+  console.log(audiArray)
+}
