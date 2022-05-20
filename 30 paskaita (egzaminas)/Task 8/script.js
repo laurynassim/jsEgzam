a5751c4cf70af52df4dc89247c66967d714ccdc6
Laurynas <simonaitis.laurynas@gmail.com>
2022-05-20 18:08:50 +0300

    8th task completed

--- 30 paskaita (egzaminas)/Task 8/script.js
+++ 30 paskaita (egzaminas)/Task 8/script.js
@@ -5,3 +5,28 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator(num1, num2){
+    this.firstNumber = num1;
+    this.secondNumber = num2;
+    this.sum = function(){
+        return this.firstNumber + this.secondNumber
+    }
+    this.subtraction = function(){
+        return this.firstNumber - this.secondNumber
+    }
+    this.multiplication = function(){
+        return this.firstNumber * this.secondNumber
+    }
+    this.division = function(){
+        return this.firstNumber / this.secondNumber
+    }
+
+}
+
+const myCalculator = new Calculator (10, 5)
+
+console.log(myCalculator.sum())
+console.log(myCalculator.subtraction())
+console.log(myCalculator.multiplication())
+console.log(myCalculator.division())
